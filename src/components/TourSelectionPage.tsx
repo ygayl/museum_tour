6f5af156811cc5da56181518e62afe5b6ac68900
Yassine Gayl <yassine.gayl@gmail.com>
2025-09-17 16:33:44 -0700

    remove theme

--- src/components/TourSelectionPage.tsx
+++ src/components/TourSelectionPage.tsx
@@ -38,7 +38,6 @@ const TourSelectionPage: React.FC<TourSelectionPageProps> = ({ tours, onSelectTo
                 <div className="absolute inset-0 bg-black bg-opacity-40 flex items-end">
                   <div className="p-4 text-white">
                     <h4 className="text-xl font-bold mb-1">{tour.name}</h4>
-                    <p className="text-sm opacity-90">{tour.theme}</p>
                   </div>
                 </div>
               </div>
