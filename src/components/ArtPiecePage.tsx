29ed6f028ba6c270e5d420613cb98c951eb6abaa
Yassine Gayl <yassine.gayl@gmail.com>
2025-09-23 15:31:27 -0700

    footer fixed

--- src/components/ArtPiecePage.tsx
+++ src/components/ArtPiecePage.tsx
@@ -93,7 +93,7 @@ const ArtPiecePage: React.FC<ArtPiecePageProps> = ({
   const currentTrackData = getCurrentTrackData();
 
   return (
-    <div className="bg-white min-h-screen pb-20">
+    <div className="bg-white min-h-screen pb-24">
       {/* Scrollable Content Area */}
       <div className="bg-white">
         {/* Hero Artwork Section */}
@@ -147,7 +147,7 @@ const ArtPiecePage: React.FC<ArtPiecePageProps> = ({
       </div>
 
       {/* Fixed Additional Content Section */}
-      <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-50">
+      <div className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 shadow-lg z-[9999]">
         <div className="px-6">
           <div className="max-w-4xl mx-auto">
             <button
